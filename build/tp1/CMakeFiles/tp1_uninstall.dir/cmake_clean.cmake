bc17480eeef2acec1c4829ccf64dc21df5d28ad5
Alex Batard <51515265+alexbatard@users.noreply.github.com>
2023-03-01 16:55:18 +0100

    Delete build directory

--- build/tp1/CMakeFiles/tp1_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tp1_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tp1_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
